324d69ba9e714cfadc52c5c52a64c1dc1b97469b
Ryan Seit <ryan@ryanseit.com>
2020-02-19 12:14:33 -0500

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,22 @@
+class Person
+    def initialize
+        @name
+        @job
+    end
+    
+    def name=(name)
+        @name = name
+    end
+
+    def job=(job)
+        @job = job
+    end
+
+    def job
+        @job
+    end
+
+    def name
+        @name
+    end
+end
